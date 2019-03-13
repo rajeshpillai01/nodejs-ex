e31b910c673f279a1ce35a720fc6b44ecff1ad60
rajeshpillai01 <rajesh.pillai01@gmail.com>
2019-03-13 11:38:09 +0530

    Updates on server.js

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ app.get('/pagecount', function (req, res) {
   }
   if (db) {
     db.collection('counts').count(function(err, count ){
-      res.send('{ pageCount: ' + count + '}');
+      res.send('{ Total pageCount: ' + count + '}');
     });
   } else {
     res.send('{ pageCount: -1 }');
